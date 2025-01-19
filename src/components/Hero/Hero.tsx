9944fca8ee8342a2e6e64fb8f52defdd9511f2af
Tulio Salvatierra <salvacorp@gmail.com>
2025-01-19 13:07:03 -0600

    update Hero and Services components with improved text formatting and styling

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 
 
+
 export default function Hero() {
   return (
     <>
       <main className="p-5  flex flex-col w-screen h-screen justify-evenly"
-        style={{ backgroundSize: '50%', backgroundPosition: 'right', backgroundRepeat: 'no-repeat'}}>
+        style={{  backgroundSize: '50%', backgroundPosition: 'right', backgroundRepeat: 'no-repeat'}}>
         <div>
           <h1 className="font-secondary font-bold text-[4rem] sm:text-[10rem] w-100 leading-tight line-clamp-2">
             Cicero Web Studio
           </h1>
           <h1 className="font-main font-thin sm:text-[1rem]">
-            by Tulio Salvatierra
+            [by Tulio Salvatierra]
           </h1>
         </div>
         <p className="font-semibold text-2xl">
--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -12,15 +12,15 @@ export default function Services() {
             [What we do]
           </h2>
         </div>
-        <ul className="flex flex-col text-2xl text-start w-1/2">
-          <li>Web Development</li>
-          <li>Mobile Development</li>
-          <li>SEO</li>
-          <li>Software Development</li>
-          <li>Consulting</li>
+        <ul className="flex flex-col text-2xl text-start sm:w-1/2 w-100">
+          <li>[01] Web Development</li>
+          <li>[02] Mobile Development</li>
+          <li>[03] SEO</li>
+          <li>[04] Software Development</li>
+          <li>[05] Consulting</li>
         </ul>
         <footer>
-          <p className="font-semibold text-2xl text-start w-1/2">
+          <p className="font-normal text-lg text-start w-3/4">
           Our mission is to deliver tailored websites and software solutions that
           solve real problems and drive meaningful growth.
           </p>
