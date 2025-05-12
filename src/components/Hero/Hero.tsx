f8e38ab0c779144ace37cabad71a6226c195edb2
Tulio Salvatierra <salvacorp@gmail.com>
2025-05-12 11:28:59 -0500

    refactor Hero component; update layout and text styles for improved readability and aesthetics

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
   return (
     <>
       <section
-        className="sm:flex w-screen h-fit justify-items-center bg-hero bg-cover overflow-hidden bg-center grid-cols-2 gap-0 p-0"
+        className="sm:flex w-screen h-100 justify-items-center bg-hero bg-cover overflow-hidden bg-center grid-cols-2 gap-0 p-0"
         style={{
           backgroundSize: "100%",
           backgroundPosition: "right",
@@ -24,22 +24,23 @@ export default function Hero() {
         </video>
         <div className="absolute top-0 left-0 w-full h-auto bg-black opacity-10"></div>
 
-        <div className="relative h-screen flex flex-col justify-center items-center p-4 w-100 sm:w-3/4">
-          <p className="font-main leading-none font-black text-[4rem] text-orange-500 sm:text-[6rem] w-100 leading-tighter">
-            We don’t just build websites, we build growth
-          </p>
+        <div className="relative flex-1 h-100 p-4 w-100 items-between">
+          <p className="font-main leading-none font-normal text-[4rem] text-orange-500 w-100 leading-tighter">
+            We don’t just build websites... 
+          </p><p className="text-[5rem] P-4 font-main font-bold leading-tighter text-orange-200 outline-4-white">WE BUILD GROWTH</p>
 
-          <h1 className="font-semibold font-secondary sm:text-3xl text-2xl text-white my-8">
+         
+        </div>
+        <div className="hidden sm:block w-1/2 h-screen bg-hero bg-cover bg-center"> <h1 className="font-semibold font-secondary sm:text-3xl text-2xl text-white my-8">
             Based in Chicago, we specialize in creating websites and software
             solutions tailored to help small businesses thrive.
           </h1>
           <a
             href={`tel:${PHONE}`}
-            className="bg-orange-500 w-full border-solid text-white rounded-full block font-bold text-xl p-6 text-center"
+            className="bg-orange-500 max-w-fit border-solid text-white rounded-full block font-bold text-xl p-6 text-center"
           >
             Get my site started now!
-          </a>
-        </div>
+          </a></div>
       </section>
     </>
   );
