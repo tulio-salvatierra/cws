f3b324911a523156ca567b2e91c75fcbf0fa9c91
Tulio Salvatierra <salvacorp@gmail.com>
2025-07-20 12:23:31 -0500

    refactor Problem component; update headings and text for improved messaging and clarity; enhance styling for better visual impact

--- src/components/Problem/Problem.tsx
+++ src/components/Problem/Problem.tsx
@@ -3,32 +3,30 @@ export default function Problem() {
     <>
       <section className="p-5  flex flex-col w-screen h-screen justify-evenly">
         <div className="flex flex-col">
-          
-          <h2 className="font-main font-black sm:text-[6rem] text-[3rem] text-zinc-700 w-100 leading-tight">
-            Problem we aim to solve:
+          <h2 className="font-main font-black sm:text-[6rem] text-[3rem] text-zinc-500 w-100 leading-tight">
+            We're not just another web design agency
           </h2>
-          <p className="font-secondary text-xl sm:text-4xl text-start sm:w-3/4">
-            Small businesses struggle to establish a credible online presence,
-            losing potential customers to competitors with modern, optimized
-            websites. <strong className="text-zinc-900">DIY can feel like:</strong>
+          <p className="font-secondary text-white text-xl sm:text-4xl text-start sm:w-3/4">
+            We're your digital groth partners. Based in Chicago, we combine
+            beautiful design with heavy metal determination to deliver solutions
+            that actually move the needle for your business
           </p>
         </div>
-        <ul className="p-4 flex flex-col text-start  w-100">
+        <ul className="p-4 flex flex-col text-start w-100 text-white">
+          <h2 className="font-main font-black sm:text-4xl text-2xl text-orange-700 w-100 leading-tight">
+            What make us different?
+          </h2>
           <li className="font-secondary text-xl sm:text-2xl p-4">
-            [01] Competitors have modern, optimized websites that attract more
-            customers.
+            Partnership approach: Your wins are our wins{" "}
           </li>
           <li className="font-secondary text-xl sm:text-2xl p-4">
-            [02] A weak or nonexistent online presence leads to missed revenue
-            opportunities.
+            Problem-solving mindset with a can-do attitude
           </li>
           <li className="font-secondary text-xl sm:text-2xl p-4">
-            [03] The business relies solely on social media, which isn’t enough
-            to establish credibility.
+            Elegant design meets relentless execution
           </li>
           <li className="font-secondary text-xl sm:text-2xl p-4">
-            [04] DIY website builders provide limited customization, making it
-            hard to stand out.
+            We go the extra mile, every single time
           </li>
         </ul>
       </section>
