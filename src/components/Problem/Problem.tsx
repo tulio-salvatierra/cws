091e1c9f6bad344c1a4b4abd495ae7c9497df0fd
Tulio Salvatierra <salvacorp@gmail.com>
2025-07-30 15:11:14 -0500

    refactor Problem component; adjust section height and typography for improved readability and visual consistency

--- src/components/Problem/Problem.tsx
+++ src/components/Problem/Problem.tsx
@@ -4,10 +4,10 @@ import CustomButton from "../CustomButton";
 export default function Problem() {
   return (
     <>
-      <section className="p-5 md:grid md:grid-cols-2 md:grid-rows-2 md:gap-y-40 gap-y-16 flex flex-col w-screen h-75 justify-evenly mt-72">
+      <section className="p-5 md:grid md:grid-cols-2 md:grid-rows-2 md:gap-y-40 gap-y-16 flex flex-col w-screen h-50 justify-evenly mt-80">
         <div className="flex flex-col justify-center items-start text-white w-full">
           <Banner />
-          <p className="font-secondary text-white text-lg text-start">
+          <p className="font-secondary text-white text-xl text-start">
             We're your digital groth partners. Based in Chicago, we combine
             beautiful design with heavy metal determination to deliver solutions
             that actually move the needle for your business
@@ -19,7 +19,7 @@ export default function Problem() {
           <h2 className="font-main font-black sm:text-4xl text-8xl text-orange-900 w-full leading-tight mb-4">
             What makes us different?
           </h2>
-          <p className="font-secondary text-lg sm:text-2xl text-slate-400 flex-1">
+          <p className="font-secondary text-lg sm:text-xl text-white flex-1">
             Partnership approach: Your wins are our wins. Problem-solving
             mindset with a can-do attitude. Elegant design meets relentless
             execution. We go the extra mile, every single time.
