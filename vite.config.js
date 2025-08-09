cc4e0b0b3f37b173c43101a83e0c41b137b77e57
Tulio Salvatierra <salvacorp@gmail.com>
2025-08-09 12:40:13 -0500

    fixed viteconfig.ts# vite-plugin-ssrvite-plugin-ssr

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+// In ESM, __dirname is not defined. Recreate it from import.meta.url
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src', // Alias para facilitar las importaciones
+      '@': path.resolve(__dirname, 'src'), // Alias para facilitar las importaciones
     },
   },
+  // Dev server and preview will both use port 5174
+  server: {
+    port: 5174,
+    strictPort: true,
+  },
+  preview: {
+    port: 5174,
+    strictPort: true,
+  },
 })
-
