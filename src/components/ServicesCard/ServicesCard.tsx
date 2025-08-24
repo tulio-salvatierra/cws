bc05b06bf15454aff003f70f8485b679ca284875
Tulio Salvatierra <salvacorp@gmail.com>
2025-08-24 18:22:57 -0500

    Refactor ServicesCard component for improved layout and responsiveness; update service images to use local assets.

--- src/components/ServicesCard/ServicesCard.tsx
+++ src/components/ServicesCard/ServicesCard.tsx
@@ -5,18 +5,18 @@ import { servicesData } from "./servicesCardData";
 export default function ServicesCard() {
   
   return (
-    <div className="mx-auto my-16">
+    <div className="flex flex-col mx-auto my-16 w-full ">
       {servicesData.map((data: { id: React.Key | null | undefined; title: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | null | undefined; description: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; image: string | undefined; }) => (
         <div
           key={data.id}
-          className="grid my-10 gap-y-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 items-center mx-auto"
+          className="grid my-10 gap-y-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 items-start mx-auto h-96"
         >
           <div className="col-span-1 flex items-start justify-start">
             <span className="text-orange-500 font-bold font-main text-6xl sm:text-7xl lg:text-8xl">
               {`[0${data.id}]`}
             </span>
           </div>
-          <div className="sm:col-span-1 lg:col-span-2 flex flex-col justify-between items-start gap-6">
+          <div className="sm:col-span-1 lg:col-span-2 flex flex-col justify-start items-start gap-6 h-full">
             <h2 className="text-orange-700 font-main text-2xl sm:text-3xl lg:text-4xl">
               {data.title}
             </h2>
@@ -25,10 +25,10 @@ export default function ServicesCard() {
             </p>
             <CustomButton secondary label="Web Development" href="#" />
           </div>
-          <div className="sm:col-span-1 lg:col-span-2 flex items-center justify-center">
+          <div className="sm:col-span-1 lg:col-span-2 flex items-start justify-center h-full">
             {data.image && (
               <img
-                className="w-full h-75 max-w-xs sm:max-w-sm lg:max-w-md object-cover"
+                className="w-full h-full max-w-xs sm:max-w-sm lg:max-w-md object-cover"
                 src={data.image}
                 alt={typeof data.title === 'string' ? data.title : 'Service Image'}
               />
